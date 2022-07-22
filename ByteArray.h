474a3ab7dd23f1b85d89c36ee5c772c05c2ba1a7
Амадей Воронцов <vorontsov.aa@phystech.edu>
2022-07-22 18:21:49 +0500

    Partially added translation to binary code

--- ByteArray.h
+++ ByteArray.h
@@ -2,13 +2,80 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include "List.h"
+#include "data_t.h"
+#include <elf.h>
 
 
 typedef unsigned long long index_t;
+typedef unsigned int addr_t;
+typedef const unsigned long long cmd_t;
+typedef const unsigned int correct_t; 
+
+
+
+namespace CMD
+{
+    cmd_t FINIT = 0x9bdbe3;
+    cmd_t PUSH_RBP = 0x55;
+    cmd_t PUSH_RAX = 0x50;
+    cmd_t MOV_RBP_RSP = 0x4889e5;
+    cmd_t SUB_RSP_NUM_L = 0x4883ec00;
+    cmd_t SUB_RSP_NUM_B = 0x4881ec00000000;
+    cmd_t ADD_RSP_NUM_L = 0x4883c400;
+    cmd_t ADD_RSP_NUM_B = 0x4881c400000000;
+    cmd_t MOV_RAX_VAR_L = 0x488b4500;
+    cmd_t MOV_RAX_VAR_B = 0x488b8500000000;
+    cmd_t MOVSD_XMM0_VAR = 0xf20f1000;
+    cmd_t MOVSD_VAR_XMM0 = 0xf20f1100;
+    cmd_t MOVSD_VAR_XMM0_L = 0xf20f114500;
+    cmd_t MOVSD_VAR_XMM0_B = 0xf20f118500000000;
+    cmd_t MOVSD_XMM0_VAR_L = 0xf20f104500;
+    cmd_t MOVSD_XMM0_VAR_B = 0xf20f108500000000;
+    cmd_t LEA_RAX_VAR_L = 0x488d4500;
+    cmd_t LEA_RAX_VAR_B = 0x488d8500000000;
+    cmd_t CALL_ADDR = 0xe8;
+    cmd_t JMP_ADDR = 0xe9;
+    cmd_t JE_ADDR = 0x0f84;
+    cmd_t JNE_ADDR = 0x0f85;
+    cmd_t JA_ADDR = 0x0f87;
+    cmd_t JAE_ADDR = 0x0f83;
+    cmd_t JB_ADDR = 0x0f82;
+    cmd_t JBE_ADDR = 0x0f86;
+    cmd_t POP_M64 = 0x8f0425;
+    cmd_t PUSH_M64 = 0xff3425;
+    cmd_t PUSH_VAR_L = 0xff7500;
+    cmd_t PUSH_VAR_B = 0xffb500000000;
+    cmd_t SQRTSD_XMM0_M64 = 0xf20f510425;
+    cmd_t MOVSD_M64_XMM0 = 0xf20f110425;
+    cmd_t MOVSD_XMM0_M64 = 0xf20f100425;
+    cmd_t MOVSD_XMM1_M64 = 0xf20f100c25;
+    cmd_t ADDSD_XMM0_XMM1 = 0xf20f58c1;
+    cmd_t SUBSD_XMM0_XMM1 = 0xf20f5cc1;
+    cmd_t MULSD_XMM0_XMM1 = 0xf20f59c1;
+    cmd_t DIVSD_XMM0_XMM1 = 0xf20f5ec1;
+    cmd_t COMISD_XMM0_XMM1 = 0x660f2fc1;
+    cmd_t LEAVE = 0xc9;
+    cmd_t RET = 0xc3;
+    cmd_t FLDZ = 0xd9ee;
+    cmd_t FSTP_M64 = 0xdd1c25;
+    cmd_t MOV_RAX_60 = 0x48c7c03c000000;
+    cmd_t XOR_RDI_RDI = 0x4831ff;
+    cmd_t SYSCALL = 0x0f05;
+    
+    correct_t CALL_CORRECT = -0x5;
+};
+
+
+
+
+void memreverse(void* begin, size_t size);
+
 
 class ByteArray
 {
 private:
+    const addr_t e_point = 0x30200000;
     char* array;
     size_t size;
     index_t cur_index;
@@ -17,7 +84,104 @@ public:
     ByteArray(const char* _array, const size_t _size);
     ~ByteArray();
 
-    index_t Append(const char ch);
+    //index_t Append(const char ch);
+    index_t Append(const Elf64_Ehdr ehdr);
+    index_t Append(const Elf64_Phdr phdr);
+    index_t Append(const uint32_t ch);
+    index_t Append(const double num);
+    //index_t Append(const uint64_t num);
+    template <typename T> index_t Append(const T cmd, size_t size); 
+
+
     const char* ByteCode() const;
     size_t Size() const;
+    void Rewind();
+    addr_t Vaddr() const
+    {
+        return e_point + cur_index;
+    }
 };
+
+
+template <typename T> index_t ByteArray::Append(const T cmd, size_t size)
+{
+    if (cur_index == size - 1)
+    {
+        array = (char*)realloc(array, 2 * size);
+        size = 2 * size;
+    }
+    memcpy(array + cur_index, &cmd, size);
+    memreverse(array + cur_index, size);
+    cur_index += size;
+    return cur_index;
+}
+
+
+class Label
+{
+private:
+    char* name;
+    addr_t addr;
+public:
+    Label();
+    Label(const Label& lbl);
+    Label(const char* _name, const addr_t _addr);
+    ~Label();
+
+    friend std::ostream& operator<< (std::ostream& stream, Label const& lbl)
+    {
+        stream << "name: " << lbl.name << "\nptr: " << lbl.addr << "\n";
+        return stream;
+    }
+
+    Label& operator=(const Label& data);
+
+    bool Contain(const char* str) const;
+    addr_t Addres() const;
+};
+
+
+struct PhdrStubs
+{
+    addr_t p_offset;
+    addr_t p_vaddp;
+    addr_t p_size;
+};
+
+
+struct EhdrStubs
+{
+    addr_t e_entry;
+    PhdrStubs rodata_stubs;
+    PhdrStubs data_stubs;
+    PhdrStubs text_stubs;
+};
+
+
+struct Stubs
+{
+    List<Label> labels;
+    EhdrStubs ElfStubs;
+    bool is_loading = true;
+    bool rewind_if = false;
+    bool rewind_const = false;
+    bool rewind_while = false;
+};
+
+
+
+addr_t LabelAddr(Stubs& st, ByteArray& code);
+Label SearchLabel(const char* funcname, List<Label> lst);
+
+void AppendCallFunc(const char* funcname, Stubs& stubs, ByteArray& machine_code);
+void AppendJmpLabel(const char* labelname, Stubs& stubs, ByteArray& machine_code);
+void AppendJxxLabel(const char* labelname, node_t* node, Stubs& stubs, ByteArray& machine_code);
+void AppendJnxLabel(const char* labelname, node_t* node, Stubs& stubs, ByteArray& machine_code);
+void AppendPushConst(int num_const, Stubs& stubs, ByteArray& machine_code);
+void AppendSubRspNum(uint32_t number, ByteArray& machine_code);
+void AppendAddRspNum(uint32_t number, ByteArray& machine_code);
+void AppendMovRaxVarPtr(uint32_t number, ByteArray& machine_code);
+void AppendMovsdVarXmm0(uint32_t number, ByteArray& machine_code);
+void AppendMovsdXmm0Var(uint32_t number, ByteArray& machine_code);
+void AppendLeaRaxVar(uint32_t number, ByteArray& machine_code);
+void AppendPushVar(uint32_t number, ByteArray& machine_code);
--- func.asm
+++ func.asm
@@ -2,704 +2,125 @@ global main
 extern printf, scanf, putchar
 
 section .data
-const_0: dq 1.000000
-const_1: dq 1.000000
-const_2: dq 1.000000
-const_3: dq 1.000000
-const_4: dq 1.000000
-const_5: dq 1.000000
-const_6: dq 1.000000
-const_7: dq 2.000000
-const_8: dq 1.000000
-const_9: dq 1.000000
-const_10: dq 2.000000
-const_11: dq 0.000000
-const_12: dq 0.000000
-const_13: dq 0.000000
-const_14: dq 2.000000
-const_15: dq 0.000000
-const_16: dq 0.000000
-const_17: dq 1.000000
-const_18: dq 0.000000
-const_19: dq 0.000000
-const_20: dq 0.000000
-const_21: dq 1.000000
-const_22: dq 0.000000
-const_23: dq 0.000000
-const_24: dq 0.000000
-const_25: dq 1.000000
-const_26: dq 2.000000
-const_27: dq 2.000000
-const_28: dq 2.000000
-const_29: dq 4.000000
-print_double: db '%lg ', 0x0
-scan_double: db '%lg', 0x0
-section .bss
-result: resq 1
+buffer: dq 0.0
+section .rodata
+const_0: dq 0.000000
+const_1: dq 23.000000
+const_2: dq 10.000000
+const_3: dq 11.000000
+const_4: dq 11.000000
+const_5: dq 11.000000
+const_6: dq 5.000000
+const_7: dq 10.000000
+const_8: dq 11.000000
+const_9: dq 0.000000
 section .text
-%define func main
+main:
+		finit
+		call	func
+		mov		rax, 60
+		xor		rdi, rdi
+		syscall
 
 func:
 		push	rbp
 		mov		rbp, rsp
-		sub		rsp, 32
-		finit
-		mov		rdi, scan_double
-		lea		rsi, [rbp - 8]
-		mov		eax, 1
-		call	scanf
-		push	qword [const_0]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 16], xmm0
-		jmp		.while0test
-.while0loop:
-		lea		rax, [rbp - 16]
-		push	rax
-		call	fib
-		add		rsp, 8
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 24], xmm0
-		mov		rdi, print_double
-		movsd	xmm0, qword [rbp - 24]
-		mov		eax, 1
-		call	printf
-		mov		rdi, 10d
-		call	putchar
-		push	qword [rbp - 16]
-		push	qword [const_1]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		addsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 16], xmm0
-.while0test:
-		push	qword [rbp - 16]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		comisd	xmm0, xmm1
-		jne		.while0loop
-.ret_func:
-		leave
-		ret
-
-fuck:
-		push	rbp
-		mov		rbp, rsp
 		sub		rsp, 16
-		mov		rax, [rbp + 16]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 8], xmm0
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [const_2]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		movsd	qword [rbp - 24], xmm0
+		push	qword [rbp - 24]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm1, qword [buffer]
 		comisd	xmm0, xmm1
-		jne		.if0else
-		push	qword [const_3]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_fuck
+		je		.if0else
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		movsd	qword [rbp - 32], xmm0
+		push	qword [rbp - 24]
+		push	qword [rbp - 32]
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm1, qword [buffer]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		mulsd	xmm0, xmm1
+		movsd	qword [buffer], xmm0
+		push	qword [buffer]
+		pop		qword [buffer]
+		movsd	xmm1, [buffer]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		addsd	xmm0, xmm1
+		movsd	qword [buffer], xmm0
+		push	qword [buffer]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		movsd	qword [rbp - 32], xmm0
+		push	qword [rbp - 24]
+		push	qword [rbp - 32]
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm1, qword [buffer]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		mulsd	xmm0, xmm1
+		movsd	qword [buffer], xmm0
+		push	qword [buffer]
+		pop		qword [buffer]
+		movsd	xmm1, [buffer]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		addsd	xmm0, xmm1
+		movsd	qword [buffer], xmm0
+		push	qword [buffer]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		movsd	qword [rbp - 32], xmm0
+		push	qword [rbp - 24]
+		push	qword [rbp - 32]
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm1, qword [buffer]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		mulsd	xmm0, xmm1
+		movsd	qword [buffer], xmm0
+		push	qword [buffer]
+		pop		qword [buffer]
+		movsd	xmm1, [buffer]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		addsd	xmm0, xmm1
+		movsd	qword [buffer], xmm0
+		push	qword [buffer]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		movsd	qword [rbp - 32], xmm0
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		movsd	qword [rbp - 32], xmm0
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		movsd	qword [rbp - 32], xmm0
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		movsd	qword [rbp - 32], xmm0
 		jmp		.if0end
 .if0else:
-		push	qword [rbp - 8]
-		push	qword [const_4]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		subsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 16], xmm0
-		push	qword [rbp - 8]
-		lea		rax, [rbp - 16]
-		push	rax
-		call	fuck
-		add		rsp, 8
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		mulsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_fuck
+		push	qword [const_0]
+		pop		qword [buffer]
+		movsd	xmm0, qword [buffer]
+		movsd	qword [rbp - 24], xmm0
 .if0end:
-.ret_fuck:
-		leave
-		ret
-
-fib:
-		push	rbp
-		mov		rbp, rsp
-		sub		rsp, 32
-		mov		rax, [rbp + 16]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 8], xmm0
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [const_5]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		comisd	xmm0, xmm1
-		jne		.if1else
-		push	qword [const_6]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_fib
-		jmp		.if1end
-.if1else:
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [const_7]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		comisd	xmm0, xmm1
-		jne		.if2else
-		push	qword [const_8]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_fib
-		jmp		.if2end
-.if2else:
-		push	qword [rbp - 8]
-		push	qword [const_9]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		subsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 16], xmm0
-		push	qword [rbp - 8]
-		push	qword [const_10]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		subsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 24], xmm0
-		lea		rax, [rbp - 24]
-		push	rax
-		call	fib
-		add		rsp, 8
-		movsd	qword [result], xmm0
-		push	qword [result]
-		lea		rax, [rbp - 16]
-		push	rax
-		call	fib
-		add		rsp, 8
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		addsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_fib
-.if2end:
-.if1end:
-.ret_fib:
-		leave
-		ret
-
-func1:
-		push	rbp
-		mov		rbp, rsp
-		sub		rsp, 32
-		push	qword [const_11]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 8], xmm0
-		push	qword [const_12]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 16], xmm0
-		push	qword [const_13]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 24], xmm0
-		mov		rdi, scan_double
-		lea		rsi, [rbp - 8]
-		mov		eax, 1
-		call	scanf
-		mov		rdi, scan_double
-		lea		rsi, [rbp - 16]
-		mov		eax, 1
-		call	scanf
-		mov		rdi, scan_double
-		lea		rsi, [rbp - 24]
-		mov		eax, 1
-		call	scanf
-		lea		rax, [rbp - 24]
-		push	rax
-		lea		rax, [rbp - 16]
-		push	rax
-		lea		rax, [rbp - 8]
-		push	rax
-		call	NumSol
-		add		rsp, 24
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 32], xmm0
-		lea		rax, [rbp - 32]
-		push	rax
-		lea		rax, [rbp - 24]
-		push	rax
-		lea		rax, [rbp - 16]
-		push	rax
-		lea		rax, [rbp - 8]
-		push	rax
-		call	Solve
-		add		rsp, 32
-.ret_func1:
-		leave
-		ret
-
-Solve:
-		push	rbp
-		mov		rbp, rsp
-		sub		rsp, 64
-		mov		rax, [rbp + 40]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 32], xmm0
-		mov		rax, [rbp + 32]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 24], xmm0
-		mov		rax, [rbp + 24]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 16], xmm0
-		mov		rax, [rbp + 16]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 8], xmm0
-		push	qword [rbp - 32]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [const_14]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		comisd	xmm0, xmm1
-		jne		.if3else
-		push	qword [const_15]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 40], xmm0
-		push	qword [const_16]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 48], xmm0
-		lea		rax, [rbp - 48]
-		push	rax
-		lea		rax, [rbp - 40]
-		push	rax
-		lea		rax, [rbp - 24]
-		push	rax
-		lea		rax, [rbp - 16]
-		push	rax
-		lea		rax, [rbp - 8]
-		push	rax
-		call	SolveSquare
-		add		rsp, 40
-		mov		rdi, print_double
-		movsd	xmm0, qword [rbp - 40]
-		mov		eax, 1
-		call	printf
-		mov		rdi, print_double
-		movsd	xmm0, qword [rbp - 48]
-		mov		eax, 1
-		call	printf
-		mov		rdi, 10d
-		call	putchar
-		jmp		.if3end
-.if3else:
-		push	qword [rbp - 32]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [const_17]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		comisd	xmm0, xmm1
-		jne		.if4end
-		push	qword [const_18]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 56], xmm0
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [const_19]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		comisd	xmm0, xmm1
-		je		.if5else
-		lea		rax, [rbp - 56]
-		push	rax
-		lea		rax, [rbp - 56]
-		push	rax
-		lea		rax, [rbp - 24]
-		push	rax
-		lea		rax, [rbp - 16]
-		push	rax
-		lea		rax, [rbp - 8]
-		push	rax
-		call	SolveSquare
-		add		rsp, 40
-		jmp		.if5end
-.if5else:
-		lea		rax, [rbp - 56]
-		push	rax
-		lea		rax, [rbp - 24]
-		push	rax
-		lea		rax, [rbp - 16]
-		push	rax
-		call	SolveLinear
-		add		rsp, 24
-.if5end:
-		mov		rdi, print_double
-		movsd	xmm0, qword [rbp - 56]
-		mov		eax, 1
-		call	printf
-		mov		rdi, 10d
-		call	putchar
-.if4end:
-.if3end:
-.ret_Solve:
-		leave
-		ret
-
-NumSol:
-		push	rbp
-		mov		rbp, rsp
-		sub		rsp, 32
-		mov		rax, [rbp + 32]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 24], xmm0
-		mov		rax, [rbp + 24]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 16], xmm0
-		mov		rax, [rbp + 16]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 8], xmm0
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [const_20]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		comisd	xmm0, xmm1
-		jne		.if6else
-		push	qword [const_21]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_NumSol
-		jmp		.if6end
-.if6else:
-		lea		rax, [rbp - 24]
-		push	rax
-		lea		rax, [rbp - 16]
-		push	rax
-		lea		rax, [rbp - 8]
-		push	rax
-		call	CalcDiscr
-		add		rsp, 24
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 32], xmm0
-		push	qword [rbp - 32]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [const_22]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		comisd	xmm0, xmm1
-		jae		.if7else
-		push	qword [const_23]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_NumSol
-		jmp		.if7end
-.if7else:
-		push	qword [rbp - 32]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		push	qword [const_24]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		comisd	xmm0, xmm1
-		jne		.if8else
-		push	qword [const_25]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_NumSol
-		jmp		.if8end
-.if8else:
-		push	qword [const_26]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_NumSol
-.if8end:
-.if7end:
-.if6end:
-.ret_NumSol:
-		leave
-		ret
-
-SolveSquare:
-		push	rbp
-		mov		rbp, rsp
-		sub		rsp, 48
-		mov		rax, [rbp + 48]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 40], xmm0
-		mov		rax, [rbp + 40]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 32], xmm0
-		mov		rax, [rbp + 32]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 24], xmm0
-		mov		rax, [rbp + 24]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 16], xmm0
-		mov		rax, [rbp + 16]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 8], xmm0
-		lea		rax, [rbp - 24]
-		push	rax
-		lea		rax, [rbp - 16]
-		push	rax
-		lea		rax, [rbp - 8]
-		push	rax
-		call	CalcDiscr
-		add		rsp, 24
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 48], xmm0
-		fldz
-		fstp	qword [result]
-		push	qword [result]
-		push	qword [rbp - 16]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		subsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		push	qword [rbp - 48]
-		pop		qword [result]
-		fld	qword [result]
-		fsqrt
-		fstp	qword [result]
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		subsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		push	qword [const_27]
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		mulsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		divsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 32], xmm0
-		fldz
-		fstp	qword [result]
-		push	qword [result]
-		push	qword [rbp - 16]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		subsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		push	qword [rbp - 48]
-		pop		qword [result]
-		fld	qword [result]
-		fsqrt
-		fstp	qword [result]
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		addsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		push	qword [const_28]
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		mulsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		divsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 40], xmm0
-.ret_SolveSquare:
-		mov		rax, [rbp + 48]
-		movsd	xmm0, qword [rbp - 40]
-		movsd	qword [rax], xmm0
-		mov		rax, [rbp + 40]
-		movsd	xmm0, qword [rbp - 32]
-		movsd	qword [rax], xmm0
-		leave
-		ret
-
-SolveLinear:
-		push	rbp
-		mov		rbp, rsp
-		sub		rsp, 32
-		mov		rax, [rbp + 32]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 24], xmm0
-		mov		rax, [rbp + 24]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 16], xmm0
-		mov		rax, [rbp + 16]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 8], xmm0
-		fldz
-		fstp	qword [result]
-		push	qword [result]
-		push	qword [rbp - 16]
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		divsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		subsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		movsd	qword [rbp - 24], xmm0
-.ret_SolveLinear:
-		mov		rax, [rbp + 32]
-		movsd	xmm0, qword [rbp - 24]
-		movsd	qword [rax], xmm0
-		leave
-		ret
-
-CalcDiscr:
-		push	rbp
-		mov		rbp, rsp
-		sub		rsp, 32
-		mov		rax, [rbp + 32]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 24], xmm0
-		mov		rax, [rbp + 24]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 16], xmm0
-		mov		rax, [rbp + 16]
-		movsd	xmm0, qword [rax]
-		movsd	[rbp - 8], xmm0
-		push	qword [rbp - 16]
-		push	qword [rbp - 16]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		mulsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		push	qword [const_29]
-		push	qword [rbp - 8]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		mulsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		push	qword [rbp - 24]
-		pop		qword [result]
-		movsd	xmm1, qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		mulsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm1, [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		subsd	xmm0, xmm1
-		movsd	qword [result], xmm0
-		push	qword [result]
-		pop		qword [result]
-		movsd	xmm0, qword [result]
-		jmp		.ret_CalcDiscr
-.ret_CalcDiscr:
+.ret_func:
 		leave
 		ret
